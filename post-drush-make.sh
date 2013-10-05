88a55e01e67943b380b70c5fbdc522151d7cb813
Ruzsa Balazs <cellux@omkamra.hu>
2013-10-05 18:35:19 +0200

    mtpk logo

--- post-drush-make.sh
+++ post-drush-make.sh
@@ -5,8 +5,11 @@ if [ -d $TARGET/translations ]; then
   mkdir -p $TARGET/profiles/mtpk/translations
   cp $TARGET/translations/*.po $TARGET/profiles/mtpk/translations
 fi
+
 mkdir -p $TARGET/sites/default/files
 chmod 777 $TARGET/sites/default/files
+
 cp $TARGET/sites/default/{default.settings.php,settings.php}
 chmod 666 $TARGET/sites/default/settings.php
 
+cp logo.png $TARGET/sites/all/themes/fold
